69bd7b7fff77a3d220a7ea9c921869ea8a392862
vivekkn91 <vivekkn91@gmail.com>
2021-06-30 18:33:34 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import ReactDom from "react-dom";
+const firstBook = [
+  { name: "vivek", age: "male" },
+  { name: "ram", age: "male" },
+  { name: "lak", age: "male" },
+  { name: "lak", age: "male" },
+  { name: "ram", age: "male" },
+];
+//const namearry = ["qdsdsd", "sdsds"];
+//const listItems = namearry.map((namee) => {
+//return <li>{namee} </li>;
+//});
+function First() {
+  {
+    // console.log(listItems);
+  }
+  return (
+    <section>
+      {firstBook.map((listofbook) => {
+        const { name, age } = listofbook;
+        return (
+          <div>
+            <h1>{name}</h1>
+            <h1>{age}</h1>
+          </div>
+        );
+      })}
+    </section>
+  );
+}
+
+ReactDom.render(<First />, document.getElementById("root"));
