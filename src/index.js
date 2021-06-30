c519e2083316dff8a2a200b16c947660524f460a
vivekkn91 <vivekkn91@gmail.com>
2021-06-30 19:12:09 +0530

    image loded

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import { render } from "@testing-library/react";
 import React from "react";
+import "./index.css";
 import ReactDom from "react-dom";
 const firstBook = [
-  { name: "vivek", age: "male" },
-  { name: "ram", age: "male" },
-  { name: "lak", age: "male" },
-  { name: "lak", age: "male" },
-  { name: "ram", age: "male" },
+  {
+    pic: "https://www.extremetech.com/wp-content/uploads/2019/12/SONATA-hero-option1-764A5360-edit.jpg",
+    name: "vivek",
+    age: "male",
+  },
+  {
+    pic: "https://www.extremetech.com/wp-content/uploads/2019/12/SONATA-hero-option1-764A5360-edit.jpg",
+    name: "ram",
+    age: "male",
+  },
+  {
+    pic: "https://www.extremetech.com/wp-content/uploads/2019/12/SONATA-hero-option1-764A5360-edit.jpg",
+    name: "lak",
+    age: "male",
+  },
+  {
+    pic: "https://www.extremetech.com/wp-content/uploads/2019/12/SONATA-hero-option1-764A5360-edit.jpg",
+    name: "lak",
+    age: "male",
+  },
+  {
+    pic: "https://www.extremetech.com/wp-content/uploads/2019/12/SONATA-hero-option1-764A5360-edit.jpg",
+    name: "ram",
+    age: "male",
+  },
 ];
 //const namearry = ["qdsdsd", "sdsds"];
 //const listItems = namearry.map((namee) => {
@@ -17,14 +38,14 @@ function First() {
     // console.log(listItems);
   }
   return (
-    <section>
+    <section className="mainsection">
       {firstBook.map((listofbook) => {
-        const { name, age } = listofbook;
+        const { pic, name, age } = listofbook;
         return (
           <div>
+            <img src={pic} />
             <h1>{name}</h1>
             <h1>{age}</h1>
-            <h3>>hello</h3>
           </div>
         );
       })}
